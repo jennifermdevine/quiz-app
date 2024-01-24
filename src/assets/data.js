e6a4e2b996fe4cffb94df960002faee46333ded9
Jennifer Devine <jennifermdevine88@gmail.com>
2024-01-23 19:56:47 -0800

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "Goldfish have a memory that lasts for how long?",
+        option1: "9 seconds",
+        option2: "10 minutes",
+        option3: "2 months",
+        option4: "6 months",
+        ans: 4,
+    },
+    {
+        question: "Test question. Select B.",
+        option1: "A",
+        option2: "B",
+        option3: "C",
+        option4: "D",
+        ans: 2,
+    },
+    {
+        question: "Test question. Select C.",
+        option1: "A",
+        option2: "B",
+        option3: "C",
+        option4: "D",
+        ans: 3,
+    },
+    {
+        question: "Test question. Select D.",
+        option1: "A",
+        option2: "B",
+        option3: "C",
+        option4: "D",
+        ans: 4,
+    },
+    {
+        question: "Test question. Select A.",
+        option1: "A",
+        option2: "B",
+        option3: "C",
+        option4: "D",
+        ans: 1,
+    }
+]
